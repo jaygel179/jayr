73bddfe1e5865e812ba60ce4a716e423dda0a928
Elpedio Adoptante Jr <eadoptante@stackify.com>
2019-06-06 19:17:46 +0800

    add index

--- backend/urls.py
+++ backend/urls.py
@@ -9,7 +9,7 @@ from django.urls import path, include
 from rest_framework import routers
 from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token
 
-from .api.views import CustomAuthToken, MessageViewSet, CalculateHolesView
+from .api.views import index_view, CustomAuthToken, MessageViewSet, CalculateHolesView
 
 router = routers.DefaultRouter()
 router.register('messages', MessageViewSet)
@@ -19,6 +19,9 @@ urlpatterns = [
     path('api/signup/', CustomAuthToken.as_view()),
     path('api/login/', obtain_jwt_token),
     path('api/verify/', verify_jwt_token),
+
+    # http://localhost:8000/
+    path('', index_view, name='index'),
 
     # http://localhost:8000/api/<router-viewsets>
     path('api/v1/', include(router.urls)),
